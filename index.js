5e73d535539688cf1061c9a5e68f663aebfd02bb
Bricz <bricz.cruz@gmail.com>
2022-07-06 17:45:32 -0500

    added views folder along with dummy homepage

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require( 'express' );
 const app = express();
+const path = require( 'path' );
+
+app.set( 'view engine', 'ejs' )
+app.set( 'views', path.join( __dirname, 'views' ) )
 
 app.listen( 3000, () => {
     console.log( "working and listening :)" );
+} )
+
+app.get( '/', ( req, res ) => {
+    res.render( 'home' );
 } )
