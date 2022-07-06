a968a9c21ce43157a588332e54f4bda5b33eb66a
Bricz <bricz.cruz@gmail.com>
2022-07-06 18:13:59 -0500

    added temp working db

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require( 'express' );
 const app = express();
 const path = require( 'path' );
+const mongoose = require( 'mongoose' );
+
+//here yap-restaurant is our temp db
+mongoose.connect( 'mongodb://localhost:2701/yap-restaurants', {} )
+
+//helps verify everything is running smoothly
+const db = mongoose.connection; //shorten code
+db.on( "error", console.error.bind( console, "connection error!" ) );
+db.once( "open", () => {
+    console.log( "lets get it started!" );
+} )
 
 app.set( 'view engine', 'ejs' )
 app.set( 'views', path.join( __dirname, 'views' ) )
