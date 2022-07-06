81cdfc1a1d37db9ac7e8ff363339651614112717
Bricz <bricz.cruz@gmail.com>
2022-07-06 18:45:06 -0500

    successfully connected a route

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const path = require( 'path' );
 const mongoose = require( 'mongoose' );
 
+const Restaurant = require( './models/restaurant' );
+
 //here yap-restaurant is our temp db
-mongoose.connect( 'mongodb://localhost:2701/yap-restaurants', {} )
+mongoose.connect( 'mongodb://localhost:27017/yap-restaurants' )
 
 //helps verify everything is running smoothly
 const db = mongoose.connection; //shorten code
@@ -16,10 +18,16 @@ db.once( "open", () => {
 app.set( 'view engine', 'ejs' )
 app.set( 'views', path.join( __dirname, 'views' ) )
 
-app.listen( 3000, () => {
-    console.log( "working and listening :)" );
-} )
-
 app.get( '/', ( req, res ) => {
     res.render( 'home' );
+} )
+
+app.get( '/addrestaurant', async ( req, res ) => {
+    const resto = new Restaurant( { title: 'KFC', priceRange: 'Budget' } );
+    await resto.save();
+    res.send( resto );
+} )
+
+app.listen( 3000, () => {
+    console.log( "working and listening :)" );
 } )
