664747220c06ad1d914db1426c79fa1136a52156
Bricz <bricz.cruz@gmail.com>
2022-07-30 22:06:30 -0500

    Reworked register logic

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,22 @@ const express = require( 'express' );
 const router = express.Router();
 const User = require('../models/user');
 
+const asyncWrapper = require('../utils/asyncWrapper')
 router.get('/register', (req, res) => {
     res.render('users/register');
-})
+});
 
-router.post('/register', async (req, res) => {
-    res.send(req.body);
-})
+router.post('/register', asyncWrapper( async (req, res) => {
+    try{
+    const {email, username, password} = req.body;
+    const user = new User({email, username});
+    const registeredUser = await User.register(user, password);
+    req.flash('success', 'Welcome to Yap');
+    res.redirect('/restaurants');
+    } catch(e){
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+}));
 
 module.exports = router;
