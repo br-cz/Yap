4c8ffa1dab3209b7e01fc13ac3485b52d23cbf84
Bricz <bricz.cruz@gmail.com>
2022-08-06 11:47:13 -0500

    hidden carousel controls when theres only 1 image

--- middleware.js
+++ middleware.js
@@ -8,7 +8,7 @@ module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()){
         //Used for redirecting back to previous page where user was prompted to login
         req.session.prevUrl = req.originalUrl;
-        console.log(req.originalUrl);
+        // console.log(req.originalUrl);
         req.flash('error', 'Not signed in');
         return res.redirect('/login');
     }
