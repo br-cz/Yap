5d6da26667b252508b5010e35283ffe5a73743ac
Bricz <bricz.cruz@gmail.com>
2022-08-07 15:15:26 -0500

    added functionality for custom geocoding instead of default

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,15 @@
+mapboxgl.accessToken = mbToken;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: restaurant.geometry.coordinates, // starting position [lng, lat]
+    zoom: 9, // starting zoom
+    projection: 'globe' // display the map as a 3D globe
+});
+map.on('style.load', () => {
+    map.setFog({}); // Set the default atmosphere style
+});
+
+new mapboxgl.Marker()
+.setLngLat(restaurant.geometry.coordinates)
+.addTo(map)
