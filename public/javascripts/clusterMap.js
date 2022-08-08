d832b2b5a2880f5999faaa6f8119224f34c4bb88
Bricz <bricz.cruz@gmail.com>
2022-08-07 20:46:38 -0500

    set cluster map data to our resto data

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -12,9 +12,8 @@ map.on( 'load', () => {
     // add the point_count property to your source data.
     map.addSource( 'earthquakes', {
         type: 'geojson',
-        // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
-        // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-        data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+        //our own custom restaurant's data
+        data: restaurants,
         cluster: true,
         clusterMaxZoom: 14, // Max zoom to cluster points on
         clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
