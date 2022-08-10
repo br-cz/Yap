dbe6b602e9d34071e86b3faa2ecf7ca476a7ff70
Bricz <bricz.cruz@gmail.com>
2022-08-10 01:13:17 -0500

    changed asyncwrapper require

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,21 +1,21 @@
 const express = require( 'express' );
 //mergeParams allows us to get access to the campground ID, since we get no access to it by default
 //as routers like to keep their params separate
-const router = express.Router({mergeParams: true}); 
-const asyncWrapper = require('../utils/AsyncWrapper');
-const ExpressError = require('../utils/ExpressError');
+const router = express.Router( { mergeParams: true } );
+const asyncWrapper = require( '../utils/asyncWrapper' );
+const ExpressError = require( '../utils/ExpressError' );
 
-const {reviewSchema} = require('../schemas.js');
-const Restaurant = require('../models/restaurant');
-const Review = require('../models/review');
+const { reviewSchema } = require( '../schemas.js' );
+const Restaurant = require( '../models/restaurant' );
+const Review = require( '../models/review' );
 
-const reviewController = require('../controllers/reviews');
+const reviewController = require( '../controllers/reviews' );
 
-const {isLoggedIn, isReviewAuthor, validateReview} = require('../middleware.js')
+const { isLoggedIn, isReviewAuthor, validateReview } = require( '../middleware.js' )
 
-router.post('/', isLoggedIn, validateReview, asyncWrapper(reviewController.createReview));
+router.post( '/', isLoggedIn, validateReview, asyncWrapper( reviewController.createReview ) );
 
 //Need reviewId to remove the reference of the review in the restaurant and the review itself
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, asyncWrapper(reviewController.deleteReview));
+router.delete( '/:reviewId', isLoggedIn, isReviewAuthor, asyncWrapper( reviewController.deleteReview ) );
 
 module.exports = router;
