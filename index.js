560e3f0bb62f4b718a2cff1159570129f87ed876
Bricz <bricz.cruz@gmail.com>
2022-08-10 00:59:28 -0500

    made start script for heroku

--- index.js
+++ index.js
@@ -158,6 +158,7 @@ app.use( ( err, req, res, next ) => {
     res.status( statusCode ).render( 'error', { err } );
 } )
 
-app.listen( 3000, () => {
+const port = process.env.PORT || 3000 //heroku's preset port
+app.listen( port, () => {
     console.log( "working and listening :)" );
 } )
